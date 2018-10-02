7ed9ff23b99fa1947543e2407e5e0fce947fbe67
Stuart McLean <stuart_mclean86@hotmail.com>
2018-10-03 00:25:34 +0200

    Ballon fix
    Ballon floating upward

---
+++ Prototype Game/Assets/Upward.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upward : MonoBehaviour
+{
+    public float floatStrength = 3.5f;
+    public Rigidbody2D rb;
+
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(Vector3.up * floatStrength);
+    }
+}
