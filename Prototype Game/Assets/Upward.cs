dfcfc3a576793430997bdab6e6b8f004a0cce85c
Stuart McLean <stuart_mclean86@hotmail.com>
2018-10-23 13:23:21 +0200

    rettelser
    en masse rettelser

--- Prototype Game/Assets/Upward.cs
+++ Prototype Game/Assets/Upward.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class Upward : MonoBehaviour
 {
-    public float floatStrength = 3.5f;
+    public float FloatStrength;
     public Rigidbody2D rb;
     public GameObject Ballon;
+    public bool ObjectDestroyed;
 
     // Use this for initialization
     void Start ()
@@ -17,14 +18,34 @@ public class Upward : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(Vector3.up * floatStrength);
+        if (ObjectDestroyed == false)
+        {
+            FloatStrength = 1.0f;
+            rb.AddForce(Vector3.up * FloatStrength);
+        }  
     }
- 
+
     void OnBecameInvisible()
     {
-
-        Instantiate(Ballon, new Vector2(Random.Range(-3f, 3f), Random.Range(-3f, 3f)), Quaternion.identity);
+        Instantiate(this.Ballon, new Vector2(Random.Range(-10f, -8f), Random.Range(-4f, -3f)), Quaternion.identity);
         Destroy(Ballon);
+        ObjectDestroyed = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("CanonBall"))
+        {
+            Rigidbody2D rocketInstance;
+            FloatStrength = 10.0f;
+            //Update();
+            //rb.AddForce(Vector3.up * FloatStrength);
+            //
+            rocketInstance = Instantiate(rb, new Vector2(Random.Range(-10f, -8f), Random.Range(-4f, -3f)), Quaternion.identity);
+            rocketInstance.velocity = new Vector3(0, FloatStrength, 0);
+            Destroy(Ballon);
+            ObjectDestroyed = true;
+        }
     }
 
 }
