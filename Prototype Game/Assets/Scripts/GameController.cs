e0fec90188fad0b682a1cc83bb77a886751df877
Stuart McLean <stuart_mclean86@hotmail.com>
2018-10-30 11:48:33 +0100

    latest changes
    Updates

--- Prototype Game/Assets/Scripts/GameController.cs
+++ Prototype Game/Assets/Scripts/GameController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    public float FloatStrength;
+    public int FloatStrength = 1;
     public Rigidbody2D rb;
     public GameObject Balloon;
     public bool ObjectDestroyed;
+
 
     // Use this for initialization
     void Start ()
@@ -16,36 +17,36 @@ public class GameController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
-        if (ObjectDestroyed == false)
-        {
-            FloatStrength = 1.0f;
-            rb.AddForce(Vector3.up * FloatStrength);
-        }  
+        rb.AddForce(Vector3.up * FloatStrength);
     }
 
     void OnBecameInvisible()
     {
-        Instantiate(this.Balloon, new Vector2(Random.Range(-10f, -8f), Random.Range(-4f, -3f)), Quaternion.identity);
+
+        FloatStrength = 1;
+
+        rb = Instantiate(rb, new Vector2(Random.Range(-10f, -8f), Random.Range(-4f, -3f)), Quaternion.identity);
+        rb.name = "Balloon"; /*Midlertidlig løsning*/
+        VelocityChange(FloatStrength);
         Destroy(Balloon);
-        ObjectDestroyed = true;
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("CannonBall"))
         {
-            Rigidbody2D rocketInstance;
-            FloatStrength = 5.0f;
-            //Update();
-            //rb.AddForce(Vector3.up * FloatStrength);
-            //
-            rocketInstance = Instantiate(rb, new Vector2(Random.Range(-10f, -8f), Random.Range(-4f, -3f)), Quaternion.identity);
-            rocketInstance.velocity = new Vector3(0, FloatStrength, 0);
+            FloatStrength = 1;
+            rb = Instantiate(rb, new Vector2(Random.Range(-10f, -8f), Random.Range(-4f, -3f)), Quaternion.identity);
+            rb.name = "Balloon";
+            VelocityChange(FloatStrength);
             Destroy(Balloon);
-            ObjectDestroyed = true;
+            Destroy(other.gameObject);
         }
     }
-
+    void VelocityChange(float Speed)
+    {
+        rb.velocity = new Vector3(0, Speed, 0);
+    }
 }
